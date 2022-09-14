aedb4c71b77f2f2953b994e8187646299fa4beba
wilsonbregeon <wilsonbregeon@gmail.com>
2022-09-13 23:29:58 -0300

    exercicios a016-lacos-II resolvidos

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const num1 = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10]
+let num2 = Number(prompt("Digite um número"))
+
+for(let i in num1){
+    console.log(`${num2} x ${num1[i]} = ${num2 * num1[i]}`)
+}
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const maioresPaises = ["1-Rússia","2 Canadá","3-China","4-EUA","5-Brasil"]
+
+for(pais of maioresPaises){
+    console.log(pais)
+}
